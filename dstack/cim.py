5269281c41fd25aadcdb1723ba4c258d4efa8bba
Rozgonyi Kristóf <rstofi@gmail.com>
2020-09-07 19:04:13 +0800

    Some extra functions are added to cim, but still need to implement moduletesting

--- dstack/cim.py
+++ dstack/cim.py
@@ -2,7 +2,7 @@
 Collection of the base functions operating on CASAImages that can be both grids and images.
 """
 
-__all__ = ['check_CIM_equity', 'measure_grid_sparseness']
+__all__ = ['check_CIM_equity', 'measure_grid_sparseness', 'measure_CIM_RMS', 'create_CIM_diff_array']
 
 import numpy as np
 
@@ -12,8 +12,7 @@ def check_CIM_equity(cimpath_a,cimpath_b,numprec=1e-8):
     """Check if two CASAImages are identical or not up to a defined numerical precision
     This function is used to test certain piepline features.
 
-    Note: NaN-s are treted as equals, and the :numprec: parameter only sets the absolute difference limit.
-    The relative difference is not used.
+    Note: NaN-s are treted as equals, and the :numprec: parameter only sets the relative difference limit.
 
     Parameters
     ==========
@@ -24,7 +23,7 @@ def check_CIM_equity(cimpath_a,cimpath_b,numprec=1e-8):
         The input CASAImage parth of Bob
 
     numprec: float
-        The numerical precision limit of the maximum allowed absolute
+        The numerical precision limit of the maximum allowed relative
         difference between CASAImages Alice and Bob.
         If set to zero, equity is checked.
 
@@ -42,8 +41,7 @@ def check_CIM_equity(cimpath_a,cimpath_b,numprec=1e-8):
     if numprec == 0.:
         return np.array_equiv(cimA.getdata(),cimB.getdata())
     else:
-        return np.allclose(cimA.getdata(),cimB.getdata(),atol=numprec,rtol=0,equal_nan=True)
-
+        return np.allclose(cimA.getdata(),cimB.getdata(),atol=0,rtol=numprec,equal_nan=True)
 
 def measure_grid_sparseness(cimgrid_path,chan=0,pol=0):
     """Measure the sparseness of the input grid, given in a complex CASAImage format
@@ -77,7 +75,6 @@ def measure_grid_sparseness(cimgrid_path,chan=0,pol=0):
     =======
     sparseness: float
         Sparseness of the grid
-
     """
     cimgrid = casaimage.image(cimgrid_path)
 
@@ -88,6 +85,97 @@ def measure_grid_sparseness(cimgrid_path,chan=0,pol=0):
     sparseness = (gird_size - np.count_nonzero(cimgrid.getdata()[chan,pol,...])) / gird_size
 
     return sparseness
+
+def create_CIM_diff_array(cimpath_a,cimpath_b,rel_diff=False,all_dim=False,chan=0,pol=0):
+    """Compute the difference of two CASAImage, and return it as a numpy array.
+    Either the entire difference cube, or only the difference of a selected channel 
+    and polarisation slice is returned.
+
+    The code computes the first minus second image given, and normalises with the
+    second one if the rel_diff parameter is set to True.
+
+    Parameters
+    ==========
+    cimpath_a: str
+        The input CASAImage parth of Alice
+
+    cimpath_b: str
+        The input CASAImage parth of Bob
+    
+    rel_diff: bool
+        If True, the relative difference is returned. The code uses Bob to normalise.
+
+    all_dim: bool
+        If True, the difference across all channels and polarisations will be computed.
+        Note taht it can be *very slow and memory heavy*!
+
+    chan: int
+        Index of the channel in the image cube
+
+    pol: int
+        Index of the polarisation in the image cube
+
+    Returns
+    =======
+    diff_array: numpy ndarray
+        Either a single channel and polarisation slice difference,
+        or the difference cube of the two input CASAImages
+    """
+    cimA = casaimage.image(cimpath_a)
+    cimB = casaimage.image(cimpath_b)
+
+    assert cimA.ndim() == cimB.ndim(), 'The dimension of the two input CASAImage is not equal!'
+
+    if all_dim:
+        if rel_diff:
+            return np.divide(np.subtract(cimA.getdata(),cimB.getdata()),cimB.getdata())
+        else:
+            return np.subtract(cimA.getdata(),cimB.getdata())
+    else:
+        if rel_diff:
+            return np.divide(np.subtract(cimA.getdata()[chan,pol,...],cimB.getdata()[chan,pol,...]),cimB.getdata()[chan,pol,...])
+        else:
+            return np.subtract(cimA.getdata()[chan,pol,...],cimB.getdata()[chan,pol,...])
+
+
+def measure_CIM_RMS(cimpath,all_dim=False,chan=0,pol=0):
+    """Measure the RMS on a CASAImage either for a given channel and polarisation,
+    or for ALL channels and polarisations. This could be very slow though.
+
+    Parameters
+    ==========
+    cimgpath: str
+        The input CASAImage parth
+
+    all_dim: bool
+        If True, the RMS will be computed for all channels and polarisations in the image cube
+        Note that, this can be *very slow*!
+
+    chan: int
+        Index of the channel in the image cube
+
+    pol: int
+        Index of the polarisation in the image cube
+
+    Returns
+    =======
+    rms: float or list of floats
+        The RMS value for the given channel or a numpy ndarray
+        containing the RMS for the corresponding channel and ploarisation
+    
+    """
+    if all_dim:
+        cimgrid = casaimage.image(cimgrid_path)
+        rms_matrix = np.zeros((cimgrid.shape()[0],cimgrid.shape()[1]))
+
+        for chan_i in range(0,cimgrid.shape()[0]):
+            for pol_j in range(0,cimgrid.shape()[1]):
+                rms_matrix[i,j] = np.sqrt(np.mean(np.square(cimgrid.getdata()[chan_i,pol_j,...])))
+
+        return rms_matrix
+
+    else:
+        return np.sqrt(np.mean(np.square(cimgrid.getdata()[chan,pol,...])))
 
 if __name__ == "__main__":
     #CIMGRID_PATH = '/home/krozgonyi/Desktop/ASKAP_high_res_grid_example/grid.wr.1.sim_PC'
