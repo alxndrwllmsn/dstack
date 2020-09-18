0a3e3bab91abb8d102ddc81da1d271b8eeb29cb2
Rozgonyi Kristóf <rstofi@gmail.com>
2020-09-18 18:28:05 +0800

    Parset module initialised but need to add to docs and init

---
+++ dstack/parset.py
@@ -0,0 +1,16 @@
+"""
+This module is a wrapper around parsets used in YandaSoft.
+The code takes template parset(s) and creates custom parsets for pipelines such as the gridded DINGO pipeline.
+The code uses the ``configparser`` package to read the template parset configuration.
+"""
+
+import configparser
+
+def some_function():
+    """Void function
+    """
+    pass;
+
+
+if __name__ == "__main__":
+    pass
