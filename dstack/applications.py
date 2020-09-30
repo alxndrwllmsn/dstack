41f48a35146ae2c4db5e02ce8a2ad29ed2c514ea
Rozgonyi Kristóf <rstofi@gmail.com>
2020-09-30 17:10:04 +0800

    Stacking application initialised

--- dstack/applications.py
+++ dstack/applications.py
@@ -22,10 +22,49 @@ def dstacking():
 
     
 
-    """    
-    print('Hello world, I am an application!')
+    """
+    parser = argparse.ArgumentParser(description='This is program to stack CASAImages')
 
+    #=== Required arguments ===
+    #The -cl option creates a list every time it is called, thus the args.cimpath_list is a list containing lists
+    #Therefore, I need to create a single list from the potential sub-lists
+    parser.add_argument('-cl', '--cimpath_list', 
+                        help='A list of the full paths of the images to be stacked', 
+                        required=True, action="append", nargs='+', type=str)
 
+    parser.add_argument('-cp', '--cim_output_path', 
+                        help='Full path to the folder in which the stacked image will be saved', 
+                        required=True, action="store", type=str)
+
+    parser.add_argument('-cn', '--cim_outputh_name', 
+                        help='Name of the stacked image', 
+                        required=True, action="store", type=str)
+
+    #=== Optional arguments ===
+    #The boolean type arguments are set to True if given, otherwise they set to False
+    parser.add_argument('-n', '--normalise', 
+                        help='If True, the images will be averaged instead of just summing them', 
+                        required=False, action="store_true")
+
+    parser.add_argument('-o', '--overwrite', 
+                        help='If True, the stacked image will be created regardless if another image exist by the same name.', 
+                        required=False, action="store_true")
+
+    parser.add_argument('-c', '--close', 
+                        help='If True the in-memory CASAIMages given by ``cimpath_list`` are deleted, and the optional write-lock releases.', 
+                        required=False, action="store_true")
+
+    #=== Application MAIN ===
+    args = parser.parse_args()
+    #Flatten out the cimpat_list argument, which currently a list of lists 
+    args.cimpath_list = [cimpath for sublist in args.cimpath_list for cimpath in sublist]
+
+    ds.cim.CIM_stacking_base(cimpath_list= args.cimpath_list,
+                            cim_output_path = args.cim_output_path,
+                            cim_outputh_name = args.cim_outputh_name,
+                            normalise = args.normalise,
+                            overwrite = args.overwrite,
+                            close = args.close)
 
 if __name__ == "__main__":
     pass
