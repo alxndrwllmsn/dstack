d2c4af074ace7ad7fad03e2b2588757c345924b3
Rozgonyi Kristóf <rstofi@gmail.com>
2021-02-02 20:37:03 +0800

    Spectra validation triangle plot added

--- validation_tools/svalidation.py
+++ validation_tools/svalidation.py
@@ -96,9 +96,68 @@ def initialise_argument_list(required_list_length,argument_list):
 
     return argument_list
 
-def plot_mom0_contour_triangle_matrix(source_ID_list, sofia_dir_list, name_base_list, output_name, N_optical_pixels=600, contour_levels=[3,5,7,9,11], masking_list=[True], mask_sigma_list=[3.0], b_maj_list=[30.], b_min_list=[30.], b_pa_list=[0.], color_list=[None], label_list=['']):
-    """
+def plot_mom0_contour_triangle_matrix(source_ID_list, sofia_dir_list, name_base_list, output_name, N_optical_pixels=600, contour_levels=[1.6,2.7,5.3,8,13,21], masking_list=[True], mask_sigma_list=[3.0], b_maj_list=[30.], b_min_list=[30.], b_pa_list=[0.], color_list=[None], label_list=['']):
+    """Create a triangle plot from the input SoFiA sources. In the plot diagonal
+    the contour plot of each source is shown. The upper triangle is empty,
+    while in the lower triangle panels both the i and j panel conbtour plots are
+    shown. This is a plot for pair-wise comparision of the input sources.
 
+    In the main diagonal the 3sigma column density sensitivity contours are shown
+    in the backround (red), while in the lower triangle only the measured column
+    density contours are shown.
+
+    The plot size is determined by the number of outputs.
+
+    The background optical image is generated based on the first source given!
+
+    Parameters
+    ==========
+    source_ID_list: list of int
+        The SoFiA ID of the sources
+
+    sofia_dir_list: list str
+        List of the SoFiA directories to compare
+
+    name_base_list: list of str
+        List of the name `output.filename` variable defined in the SoFiA template 
+        .par. Basically the base of all file names in the rspective SoFiA dir.
+
+    output_name: str
+        The name and full path to the output tiangle plot generated.
+
+    N_optical_pixels: int
+        Number of pixels for the optical background image.
+
+    contour_levels: list of float
+        List containing the column density contours to drawn in units of 10^(20)
+        HI atoms / cm^2.
+
+    masking_list: list of bool
+        If True, the respective mom0 maps will be msked
+
+    mask_sigma_list: list of float
+        If the mom0 map is masked, pixels below this threshold will be masked.
+        The values are given in units of column density sensitivity.
+
+    b_maj_list: list of float
+        The major axis of the synthesised beam in arcseconds.
+
+    b_min_list: list of float
+        The minor axis of the synthesised beam in arcseconds.
+
+    b_pa_list: list of float
+        The position angle of the synthesised beam.
+
+    color_list: list of colors
+        A list defining each sources color. If None is given a random color is
+        generated.
+
+    label_list: list of str
+        The name/titile used for each source.
+    Return
+    ======
+    output_image: file
+        The image created
     """
     #Initialise arguments by recursively appending them
     N_sources = len(source_ID_list)
@@ -234,9 +293,251 @@ def plot_mom0_contour_triangle_matrix(source_ID_list, sofia_dir_list, name_base_
     plt.close()
 
 
+def plot_spectra_triangle_matrix(source_ID_list, sofia_dir_list, name_base_list, output_name, beam_correction_list=[True], b_maj_px_list=[5.], b_min_px_list=[5.0], v_frame_list=['optical'], color_list=[None], label_list=[''], ident_list=['?']):
+    """Plot a triangle matrix of the spectra from different SoFiA runs. The diagonal
+    panels shows the spectra of each source. The upper triangle shows the relative
+    difference of the respectibe spectras (i,j). While the lower panel simply
+    shows two spectras on top of each other.
 
+    Parameters
+    ==========
+    source_ID_list: list of int
+        The SoFiA ID of the sources
 
+    sofia_dir_list: list str
+        List of the SoFiA directories to compare
 
+    name_base_list: list of str
+        List of the name `output.filename` variable defined in the SoFiA template 
+        .par. Basically the base of all file names in the rspective SoFiA dir.
+
+    output_name: str
+        The name and full path to the output tiangle plot generated.
+
+    N_optical_pixels: int
+        Number of pixels for the optical background image.
+
+    contour_levels: list of float
+        List containing the column density contours to drawn in units of 10^(20)
+        HI atoms / cm^2.
+
+    beam_correction_list: list of bool
+        If True, the spectra is converted from Jy/beam to Jy by normalising with
+        the synthesiesd beam area
+
+    b_maj_px_list: list of float
+        The major axis of the synthesised beam in pixels.
+
+    b_min_px_list: list of float
+        The minor axis of the synthesised beam in pixels.
+
+    v_frame_list: list of str
+        The velocity frame used for each spectra. Has to be the same for all.
+
+    color_list: list of colors
+        A list defining each sources color. If None is given a random color is
+        generated.
+
+    label_list: list of str
+        The name/titile used for each source.
+    
+    ident_list: list of str
+        A list containing only a single character. This will be used in the top
+        panel to indicate which spectra is subtracted from which. It also used to
+        append the label list with this string in parentheses.
+    
+    Return
+    ======
+    output_image: file
+        The image created
+    """
+
+    #Initialise arguments by recursively appending them
+    N_sources = len(source_ID_list)
+    assert len(sofia_dir_list) == N_sources, 'More or less sources are given than SoFiA directory paths!'
+
+    beam_correction_list = initialise_argument_list(N_sources,beam_correction_list)
+    b_maj_px_list = initialise_argument_list(N_sources, b_maj_px_list)
+    b_min_px_list = initialise_argument_list(N_sources, b_min_px_list)
+    v_frame_list = initialise_argument_list(N_sources, v_frame_list)
+    label_list = initialise_argument_list(N_sources, label_list)
+    ident_list = initialise_argument_list(N_sources, ident_list)
+
+    #All the velocity frames has to be the same
+    if all(vf == v_frame_list[0] for vf in v_frame_list) == False:
+        raise ValueError('Different velocity frames are defined for the input spectars!')
+
+    #The name bases might be the same
+    if len(name_base_list) != N_sources:
+        name_base_list = initialise_argument_list(N_sources, name_base_list)
+
+    #Generate random colors if needed
+    for i in range(0,N_sources):
+        if color_list[i] == None:
+            color_list[i] = "#{:06x}".format(random.randint(0, 0xFFFFFF)) #Generate random HEX color
+
+    #Get the spectra and velocity arrays
+    flux_list = []
+    velocity_list = []
+
+    for i in range(0,N_sources):
+
+        flux, velocity = ds.sdiagnostics.get_spectra_array(source_ID = source_ID_list[i],
+                                        sofia_dir_path = sofia_dir_list[i],
+                                        name_base = name_base_list[i],
+                                        v_frame = v_frame_list[i],
+                                        beam_correction = beam_correction_list[i],
+                                        b_maj_px = b_maj_px_list[i],
+                                        b_min_px = b_min_px_list[i])
+
+        flux_list.append(flux)
+        velocity_list.append(velocity)
+
+    flux_list = np.array(flux_list)
+    velocity_list = np.array(velocity_list)
+    
+    #Get the difference arrays of the spectra for the upper triangle
+    #Map the i,j indices to a list as: i *  N_sources + j
+    isum = []
+    diff_array_list = []
+    v_index_list = []
+    subtraction_ident_list = []
+    
+    #for the common diff y-axis
+    diff_max = -np.inf
+    diff_min = np.inf
+
+    for i in range(0,N_sources):
+        for j in range(0,N_sources):
+            if i<j:
+                #Here I create the difference arrays by subtracting the flux value
+                # of array b from the flux of a, using the closest velocity channel
+                # value of b. I have the assumption that the end of the spectra is
+                # always zero (or beginning). Thus, this closest channel subtraction
+                # works if the two arrays have different number of channels, or
+                # even if they spanning over different velocity regions.
+                # This is a slow but quite robust method.
+                
+                if np.size(flux_list[i]) >= np.size(flux_list[j]):
+                    fa = flux_list[i]
+                    va = velocity_list[i]
+                    
+                    fb = flux_list[j]
+                    vb = velocity_list[j]
+
+                    v_index_list.append(True)
+
+                else:
+                    fa = flux_list[j]
+                    va = velocity_list[j]
+                    
+                    fb = flux_list[i]
+                    vb = velocity_list[i]
+
+                    v_index_list.append(False)
+
+                diff_flux = np.zeros(np.size(fa))
+                for k in range(0, np.size(fa)):
+                    v_index = np.argmin(np.fabs(np.subtract(va[k],vb)))
+                    
+                    if np.size(flux_list[i]) >= np.size(flux_list[j]):
+                        diff_flux[k] = np.subtract(fa[k], fb[v_index])
+                    else: 
+                        diff_flux[k] = np.subtract(fb[v_index], fa[k])
+ 
+                if diff_min > np.amin(diff_flux):
+                    diff_min = np.amin(diff_flux)
+
+                if diff_max < np.amax(diff_flux):
+                    diff_max = np.amax(diff_flux)
+
+                isum.append(int(N_sources * i + j))
+                diff_array_list.append(diff_flux)
+                
+                #Note that we always subtract j from i (indices) !
+                subtraction_ident_list.append('({0:s} - {1:s})'.format(
+                            ident_list[i], ident_list[j]))
+ 
+    #=== Create the figure
+    fig, axes = plt.subplots(figsize=(2 + 4 * N_sources, 2 + 4 * N_sources),
+                sharex=True, sharey=True, ncols=N_sources, nrows=N_sources)
+
+    for i in range(0,N_sources):
+        for j in range(0,N_sources):
+            if i<j:
+
+                if int(N_sources * i + j) in isum:
+                    panel_index = np.where(np.array(isum) == int(N_sources * i +j))[0][0]
+                    
+                    if v_index_list[panel_index]:
+                        va = velocity_list[i]
+                    else:
+                        va = velocity_list[j]
+
+                    ax = axes[i,j].twinx()
+
+                    #set grid
+                    ax.grid(c='black', ls='--', alpha=0.5, lw=1.5)
+                    axes[i,j].grid(axis='x', lw=1.5, alpha=0.5)
+
+                    #Plot
+                    ax.step(va, diff_array_list[panel_index], c='red', linewidth=2., alpha=0.5)
+                
+                    ax.set_ylim((diff_min,diff_max))
+                    
+                    #Add inner title
+                    t = ds.sdiagnostics.add_inner_title(ax,
+                            subtraction_ident_list[panel_index],loc=2,
+                            prop=dict(size=16, color='red'))
+                    t.patch.set_ec("none")
+                    t.patch.set_alpha(0.5)
+
+                   
+                    #set ticks
+                    axes[i,j].tick_params(length=0.) #Remove ticks
+                    
+                    if j != (N_sources - 1):
+                        ax.tick_params(length=0.)
+                        ax.set(yticklabels=[])
+                        
+                    else:
+                        ax.set_ylabel(r'$\Delta$S', fontsize=18)
+
+            else:
+                axes[i,j].step(velocity_list[i],flux_list[i],
+                c=color_list[i], linewidth=2.5, alpha=0.8)
+
+                axes[i,j].grid(lw=1.5, alpha=0.5)
+                axes[i,j].tick_params(length=0.) #Remove ticks
+
+                if i != j:
+                        axes[i,j].step(velocity_list[j], flux_list[j],
+                                c=color_list[j], linewidth=2.5, alpha=0.8)
+
+                        axes[i,j].grid(lw=1.5, alpha=0.5)
+                        axes[i,j].tick_params(length=0.) #Remove ticks
+                
+                else:
+                    #Add inner title
+                    t = ds.sdiagnostics.add_inner_title(axes[i,j], label_list[i] \
+                            + ' ({0:s})'.format(ident_list[i]), loc=2, 
+                            prop=dict(size=16, color=color_list[i]))
+                    t.patch.set_ec("none")
+                    t.patch.set_alpha(0.5)
+
+            if i == (N_sources - 1):
+                axes[i,j].set_xlabel('v$_{opt}$ [km/s]', fontsize=18)
+                axes[i,j].tick_params(axis='x', length=matplotlib.rcParams['xtick.major.size'])
+
+            if j == 0:
+                axes[i,j].set_ylabel(r'S [Jy]', fontsize=18)
+                axes[i,j].tick_params(axis='y', length=matplotlib.rcParams['xtick.major.size'])
+
+    #Some style settings
+    plt.subplots_adjust(left=None, bottom=None, right=None, top=None, wspace=0.0, hspace=0.0)
+
+    plt.savefig(output_name, bbox_inches='tight')
+    plt.close()
 
 
 #=== MAIN ===
@@ -245,7 +546,56 @@ if __name__ == "__main__":
     log.setLevel(logging.INFO)
     log.addHandler(logging.StreamHandler(sys.stdout))
 
-    """
+    #"""
+    #2km baselines
+    working_dir = '/home/krozgonyi/Desktop/quick_and_dirty_sofia_outputs/'
+
+    sofia_dir_path_list = list(map(working_dir.__add__,['co_added_visibilities/',
+        'stacked_grids/', 'stacked_images/', 'conventional_imaging/']))
+
+    log.info('Creating spectra triangle plot for 2km baselie results...')
+
+
+    plot_spectra_triangle_matrix(source_ID_list = [1, 1, 1, 2],
+            sofia_dir_list = sofia_dir_path_list,
+            name_base_list = ['beam17_all_'],
+            output_name = working_dir + 'validation/spectras.pdf',
+            beam_correction_list = [True, True, True, False],
+            b_maj_px_list = [5.0],
+            b_min_px_list = [5.0],
+            color_list = [c0, c2, c1, outlier_color],
+            label_list = ['co-added visibilities', 'stacked grids', 'stacked images', 'conventional imaging'],
+            ident_list = ['V', 'G', 'I', 'C'])
+
+    log.info('...done')
+
+    exit()
+    #"""
+
+    #6km baselines
+    working_dir = '/home/krozgonyi/Desktop/quick_and_dirty_sofia_outputs/high_resolution/'
+
+    sofia_dir_path_list = list(map(working_dir.__add__,['co_added_visibilities/',
+        'stacked_grids/', 'stacked_images/']))
+
+    log.info('Creating spectra triangle plot for 6km baselie results...')
+
+    plot_spectra_triangle_matrix(source_ID_list = [1, 1, 1],
+            sofia_dir_list = sofia_dir_path_list,
+            name_base_list = ['beam17_all_'],
+            output_name = working_dir + 'validation/spectras.pdf',
+            beam_correction_list = [True, True, True],
+            b_maj_px_list = [6.0],
+            b_min_px_list = [6.0],
+            color_list = [c0, c2, c1],
+            label_list = ['co-added visibilities', 'stacked grids', 'stacked images'],
+            ident_list = ['V', 'G', 'I'])
+
+    log.info('...done')
+
+    exit()
+
+    #"""
     #2km baselines
     working_dir = '/home/krozgonyi/Desktop/quick_and_dirty_sofia_outputs/'
 
@@ -271,7 +621,7 @@ if __name__ == "__main__":
     exit()
     #"""
 
-    """
+    #"""
     #6km baselines
     working_dir = '/home/krozgonyi/Desktop/quick_and_dirty_sofia_outputs/high_resolution/'
 
@@ -295,7 +645,7 @@ if __name__ == "__main__":
     exit()
     #"""
 
-    """
+    #"""
     #Chiles example SoFiA analysis
     ds.sdiagnostics.create_complementary_figures_to_sofia_output(
         sofia_dir_path = '/home/krozgonyi/Desktop/chiles_example/runSoFiA/',
